13500a7918646e43a07212f12eef974302f834fa
Darkmintis <darkmintis@gmail.com>
2026-09-13 18:04:52 +0545

    feat: update plugin registrations for media kit and volume controller

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  connectivity_plus
-  url_launcher_windows
+  media_kit_libs_windows_video
+  media_kit_video
+  volume_controller
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
